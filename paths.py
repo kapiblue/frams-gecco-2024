eef04ffdcfb6ed85bf8e07c788e57b0da1e8d012
Bartosz Stachowiak <sbartekt@op.pl>
2024-06-12 20:49:21 +0200

    extend paths with config for linux

--- paths.py
+++ paths.py
@@ -1,7 +1,15 @@
 import pathlib
+import sys
+
+
+is_win = sys.platform.startswith("win")
 
 FRAMSPY_DIR = pathlib.Path(__file__).parent.parent.resolve()
 FRAMS_GECCO_DIR = pathlib.Path(__file__).parent.resolve()
 RESULTS_DIR = FRAMS_GECCO_DIR / "results"
-INTERPRETER = FRAMSPY_DIR.parent / ".venv/Scripts/python.exe"
-FRAMLIB_DIR = "D:\\Programs\\Framsticks\\"
+if is_win:
+    INTERPRETER = FRAMSPY_DIR.parent / ".venv/Scripts/python.exe"
+    FRAMLIB_DIR = "D:\\Programs\\Framsticks\\"
+else:
+    INTERPRETER = "python"
+    FRAMLIB_DIR = "/home/sbartekt/Framsticks50rc30/"
